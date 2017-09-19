b3393494f25dee910c79558205f5ec18854863ba
Juliana Gomez <juliana@gomezjuliana.com>
2017-09-18 20:52:05 -0500

    connects API and gets location data

--- 003-servers-and-maps/src/app/commons/weather.factory.js
+++ 003-servers-and-maps/src/app/commons/weather.factory.js
@@ -1,23 +1,24 @@
-{
+(function() {
+  'use strict';
+
 	angular
 		.module('angularApp')
 		.factory('WeatherFactory', WeatherFactory);
 
 	/** @ngInject **/
-	function WeatherFactory($resource, $filter, $rootScope, WEATHER_API){
-		var vm = this;
-
-		vm.weather = {
+	function WeatherFactory($http, $q){
+		return {
 			getWeather: getWeather
-		};
+		}
 
-		function getWeather(lat, lon) {
-			return $resource('http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_API+'&lat='+lat+'&lon='+lon, {}, {
-				'query': {
-					method: 'GET'
-				}
+		function getWeather(lat, lng){
+			return $http.get(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=534eccb946ce639dbb41f82b8be15dcc`)
+			.success(function(response){
+				return response;
+			})
+			.catch(function(error){
+				return $q.reject(error)
 			});
 		}
-		return vm.weather;
 	}
-}
+})();
--- 003-servers-and-maps/src/app/states/weather/weather.controller.js
+++ 003-servers-and-maps/src/app/states/weather/weather.controller.js
@@ -9,15 +9,12 @@
   function WeatherController($http, $scope, WeatherFactory) {
     var vm = this;
 
-    //Talitos API Key
-    vm.apiKey = '534eccb946ce639dbb41f82b8be15dcc';
     vm.kind = '0';
 
-    //Should we extend or use vm?
     angular.extend($scope, {
         center: {
-            lat: 43.4821971,
-            lng: -80.6167592,
+            lat: 38.8225909761771,
+            lng: -96.5478515625,
             zoom: 4
         },
         defaults: {
@@ -31,26 +28,17 @@
         }
     });
 
-    $scope.$on('leafletDirectiveMap.map.click', function(event, args){
-        WeatherFactory.getWeather();
+    $scope.$on('leafletDirectiveMap.map.click', getLocation);
 
-        // if(vm.kind == '0'){
-        //     //Remember Services
-        //     $http({
-        //         method: 'GET',
-        //         url: 'http://api.openweathermap.org/data/2.5/home?APPID='+vm.apiKey+'&q=London'
-        //     }).then(function successCallback(response) {
-        //         alert(response.data.name); //Not a good practice 
-        //     }, function errorCallback(response) {
-        //         console.log('Error');
-        //     });
-        // }else if(vm.kind == '1'){
-        //     //http://api.openweathermap.org/v3/uvi/{lat},{lon}./current.json?appid={your-api-key}
-        // }else if(vm.kind == '2'){
-        //     //http://api.openweathermap.org/pollution/v1/co/{location}/current.json?appid={api_key}
-        // }
-    });
 
+    function getLocation(event, args){
+
+        $scope.center.lat = args.leafletEvent.latlng.lat;
+        $scope.center.lng = args.leafletEvent.latlng.lng;
+        
+        WeatherFactory.getWeather($scope.center.lat, $scope.center.lng).then(function(response){
+            console.log(response.data);
+        });
+    }
   }
-
 })();
