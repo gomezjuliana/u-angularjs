9d27764693fc8deb6fab133266e17f28dedec5e0
Juliana Gomez <juliana@gomezjuliana.com>
2017-09-26 16:53:37 -0500

    working on making the favoruites list re-viewable

--- 004-directives/src/app/states/home/home.controller.js
+++ 004-directives/src/app/states/home/home.controller.js
@@ -14,13 +14,27 @@
     vm.card = {};
     vm.card.setFavorite = setFavorite;
     vm.setIcon = setIcon;
+    vm.resetFavorite = resetFavorite
 
-    function setFavorite(item){
-      vm.favoriteList.push(item);
+    function setFavorite(title, description, background, textColor, icon){
+      var fave = {
+        title: title,
+        description: description,
+        background: background,
+        textColor: textColor,
+        icon: icon
+      }
+      vm.favoriteList.push(fave);
+      console.log(vm.favoriteList)
     }
 
     function setIcon(iconClass) {
       vm.card.icon = iconClass;
+      console.log(vm.card.icon)
+    }
+
+    function resetFavorite() {
+      console.log('hey')
     }
   }
 
