ce7fab6441175fe9470b0bf43a1eb779a183fda2
Juliana Gomez <juliana@gomezjuliana.com>
2017-10-23 20:50:00 -0500

    upload homework

--- 007-end-to-end/e2e/main.po.js
+++ 007-end-to-end/e2e/main.po.js
@@ -7,8 +7,18 @@
 
 var MainPage = function() {
 
+  this.icon = element(by.css('body')).all(by.repeater('icon in homeCtrl.icons')).first();
+  this.background = element.all(by.css('.md-color-picker-input')).first();
+  this.textColor = $$('.md-color-picker-input').last();
   this.title = element(by.model('homeCtrl.card.title'));
+  this.description = element(by.model('homeCtrl.card.description'));
+  this.setFavorite = element(by.css('#favoriteBtn'));
+  this.favoriteList = element(by.css('body')).all(by.repeater('favorite in homeCtrl.favoriteList'));
+
+  this.directive = element(by.css('#card'));
   this.directiveTitle = element(by.css('#cardTitle'));
+  this.descriptionDescription = element(by.css('#cardDescription'));
+  this.directiveReservedBy = element(by.css('#cardReservedBy'));
 
 };
 
--- 007-end-to-end/e2e/main.spec.js
+++ 007-end-to-end/e2e/main.spec.js
@@ -4,7 +4,12 @@ describe('The main view', function () {
   var page;
 
   function fillForm() {
+    page.icon.click();
+    page.icon.sendKeys('home');
+    page.background.sendKeys('orange');
+    page.textColor.sendKeys('white');
     page.title.sendKeys('Talos Business Card');
+    page.description.sendKeys('Juliana Gomez - October 2017');
   }
 
   beforeEach(function () {
@@ -14,11 +19,16 @@ describe('The main view', function () {
 
   it('should be right data into directive', function() {
     fillForm();
+
+    expect(page.directive.isPresent()).toBe(true);
     expect(page.directiveTitle.evaluate('title').isPresent()).toBe(true);
+    expect(page.directiveDescription.evaluate('description').isPresent()).toBe(true);
   });
 
   it('should favorite list equal to 1', function () {
-    //other test here
+    fillForm();
+    page.setFavorite.click();
+    expect(page.favoriteList.count()).toEqual(1);
   });
 
 });
